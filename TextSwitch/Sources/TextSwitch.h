53958b911cf3fce3113fe76aa1abf7ab6d9da581
Marcelo Vitoria <marcvit@gmail.com>
2018-12-06 11:08:05 -0200

    Initial commit

---
+++ TextSwitch/Sources/TextSwitch.h
@@ -0,0 +1,19 @@
+//
+//  TextSwitch.h
+//  TextSwitch
+//
+//  Created by Marcelo Vitoria on 06/12/18.
+//  Copyright © 2018 Marcelo Vitoria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextSwitch.
+FOUNDATION_EXPORT double TextSwitchVersionNumber;
+
+//! Project version string for TextSwitch.
+FOUNDATION_EXPORT const unsigned char TextSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextSwitch/PublicHeader.h>
+
+
---
+++ TextSwitchSample/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextSwitchSample
+//
+//  Created by Marcelo Vitoria on 06/12/18.
+//  Copyright © 2018 Marcelo Vitoria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
