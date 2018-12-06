c82931c8b9381752b73e5ebcb60800d071a1eacc
Marcelo Vitoria <marcvit@gmail.com>
2018-12-06 12:34:59 -0200

    Change sample to set control's background colors

--- TextSwitchSample/Sources/ViewController.swift
+++ TextSwitchSample/Sources/ViewController.swift
@@ -15,7 +15,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let colorWhenOn = UIColor(red: 126/255, green: 223/255, blue: 179/255, alpha: 1)
+        let colorWhenOff = UIColor(red: 1, green: 154/255, blue: 155/255, alpha: 1)
+
         self.textSwitch = UITextSwitch()
+        self.textSwitch.trailColorWhenOn = colorWhenOn
+        self.textSwitch.trailColorWhenOff = colorWhenOff
+        self.textSwitch.textWhenOn = "Do it!"
+        self.textSwitch.textWhenOff = "Don't do it!"
 
         self.view.addSubview(self.textSwitch)
     }
@@ -26,4 +33,3 @@ class ViewController: UIViewController {
         self.textSwitch.center = self.view.center
     }
 }
-
